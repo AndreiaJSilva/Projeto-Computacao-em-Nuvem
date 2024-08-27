eed9a0f7650f5743331ef276d8ca4655199442b3
pedro.siqueira1 <pedro.siqueira1@estudante.ufla.br>
2024-08-27 18:39:17 -0300

    Atualizando

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,10 @@
 const express = require('express');
-<<<<<<< HEAD
 const path = require('path');
-=======
->>>>>>> fb2c6669f0d7b3308d475f5938bcef498a521bae
 const app = express();
 const connection = require('./db'); // importar conexão com o banco de dados
 const cors = require('cors'); 
 
-<<<<<<< HEAD
 app.use(express.static(path.join(__dirname, '../frontend')));
-=======
-// app.use(express.static(path.join(__dirname, 'frontend')));
->>>>>>> fb2c6669f0d7b3308d475f5938bcef498a521bae
 
 // Middleware para JSON
 app.use(express.json());
@@ -41,7 +34,6 @@ app.post('/clientes', (req, res) => {
   });
 });
 
-<<<<<<< HEAD
  app.put('/clientes/:id', (req, res) => {
    const { id } = req.params;
    const { nome, email } = req.body;
@@ -66,32 +58,6 @@ app.post('/clientes', (req, res) => {
      res.json({ message: 'Cliente deletado com sucesso' });
    });
  });
-=======
-// app.put('/clientes/:id', (req, res) => {
-//   const { id } = req.params;
-//   const { nome, email } = req.body;
-//   const sql = 'UPDATE clientes SET nome = ?, email = ? WHERE id = ?';
-  
-//   connection.query(sql, [nome, email, id], (err, result) => {
-//     if (err) {
-//       return res.status(500).json({ error: err.message });
-//     }
-//     res.json({ message: 'Cliente atualizado com sucesso' });
-//   });
-// });
-
-// app.delete('/clientes/:id', (req, res) => {
-//   const { id } = req.params;
-//   const sql = 'DELETE FROM clientes WHERE id = ?';
-  
-//   connection.query(sql, [id], (err, result) => {
-//     if (err) {
-//       return res.status(500).json({ error: err.message });
-//     }
-//     res.json({ message: 'Cliente deletado com sucesso' });
-//   });
-// });
->>>>>>> fb2c6669f0d7b3308d475f5938bcef498a521bae
 
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
